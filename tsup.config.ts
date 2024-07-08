745f5e4c0ff574e403c8d7aadfb865a950022cc7
JinmingYang <2214962083@qq.com>
2024-07-08 23:26:24 +0800

    build: update build config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,24 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['cjs'],
-  shims: false,
-  dts: false,
-  external: ['vscode']
-})
+export default defineConfig(async () => [
+  {
+    name: 'node',
+    entry: ['src/index.ts'],
+    format: ['cjs'],
+    shims: false,
+    dts: false,
+    outDir: 'dist/node',
+    platform: 'node',
+    target: 'node18',
+    sourcemap: true,
+    clean: true,
+    define: {
+      'process.env.IS_BROWSER': 'false'
+    },
+    treeshake: {
+      preset: 'smallest',
+      moduleSideEffects: 'no-external'
+    },
+    external: ['vscode']
+  }
+])
