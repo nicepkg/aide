80f8c58da437a9ecdba062f91e6663c3d0d8c12a
JinmingYang <2214962083@qq.com>
2024-08-05 01:39:11 +0800

    fix: fix vscode 1.92.0 fetch not working

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -1,5 +1,6 @@
 const enableFetchPolyfill = async () => {
   if (!globalThis.fetch) {
+    // if globalThis.fetch is not available, we use undici
     const { fetch, FormData, Headers, Request, Response, File } = await import(
       'undici'
     )
@@ -13,6 +14,24 @@ const enableFetchPolyfill = async () => {
       File
     })
   }
+
+  // fuck, vscode fetch not working on v1.92.0
+  // we add a polyfill here
+  const {
+    default: fetch,
+    Headers,
+    Request,
+    Response
+  } = await import('node-fetch')
+  const { default: FormData } = await import('form-data')
+
+  Object.assign(globalThis, {
+    fetch,
+    FormData,
+    Headers,
+    Request,
+    Response
+  })
 }
 
 export const enablePolyfill = async () => {
